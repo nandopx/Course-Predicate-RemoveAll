a99666d407f70cae6ea4c199bc2c6ae7790428c1
Jesse Fernando <nandopox@gmail.com>
2021-03-19 22:38:13 -0300

    Working with axiliary method

--- Course-Predicate-RemoveAll/Program.cs
+++ Course-Predicate-RemoveAll/Program.cs
@@ -14,12 +14,17 @@ namespace Course_Predicate_RemoveAll
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            list.RemoveAll(p => p.Price >= 100.0);
+            list.RemoveAll(ProductTest);
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
         }
     }
 }
