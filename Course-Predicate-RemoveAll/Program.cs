1af606432cd42c0fffdeceaa9c7bab7db36611b5
Jesse Fernando <nandopox@gmail.com>
2021-03-19 22:25:42 -0300

    Project created

---
+++ Course-Predicate-RemoveAll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Course_Predicate_RemoveAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
